5945f69de15d20080863a4a961032de6d22b3771
btl <bertrand.thill@francetv.fr>
2020-11-25 14:42:42 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name = "thumbor_ftvnum_libs",
-    version = "0.1.0",
+    version = "0.1.1",
     description = "libs thumbor",
     author = "Bertrand Thill",
     author_email = "bertrand.thill@francetv.fr",
@@ -33,10 +33,7 @@ setup(
                    'Topic :: Multimedia :: Graphics :: Presentation'
     ],
     package_dir = {"thumbor_ftvnum_libs": "thumbor_ftvnum_libs"},
-    install_requires=[
-        'thumbor>=6.7.0',
-        'pymongo>=3.4.0'
-    ]
+    install_requires=['thumbor>=6.7.0','pymongo>=3.4.0'],
     long_description = """\
 This module test support for file.
 """
