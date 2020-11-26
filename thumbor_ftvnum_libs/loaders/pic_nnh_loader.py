a6106c86e592e4ad3e3f5f7840aa6ec91d8f0e41
btl <bertrand.thill@francetv.fr>
2020-11-26 17:43:00 +0100

    patch # pas de http request si le protocole n'est pas present dans le path (si c'est du file)

--- thumbor_ftvnum_libs/loaders/pic_nnh_loader.py
+++ thumbor_ftvnum_libs/loaders/pic_nnh_loader.py
@@ -13,6 +13,7 @@ import urllib
 from thumbor.loaders import http_loader
 from thumbor_ftvnum_libs.loaders import pic_nn_loader
 from tornado.concurrent import return_future
+from thumbor.loaders import LoaderResult
 
 @return_future
 def load(context, path, callback):
@@ -21,7 +22,13 @@ def load(context, path, callback):
             callback(result)
         else:
             # If file_loader failed try http_loader
-            http_loader.load(context, path, callback)
+            if (path.find('http') != -1):
+                http_loader.load(context, path, callback)
+            else:
+                result = LoaderResult()
+                result.error = LoaderResult.ERROR_NOT_FOUND
+                result.successful = False
+                callback(result)
 
     # First attempt to load with file_loader
     pic_nn_loader.load(context, path, callback_wrapper)
