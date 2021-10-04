de2609d12754eedf79646a6bdeade7fca72b6129
btl <bertrand.thill@francetv.fr>
2021-10-04 13:52:24 +0200

    Fix mongo Bson

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name = "thumbor_ftvnum_libs",
-    version = "0.1.6",
+    version = "0.1.7",
     description = "libs thumbor",
     author = "Bertrand Thill",
     author_email = "bertrand.thill@francetv.fr",
